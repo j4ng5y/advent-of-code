8d9328b3a5860f2a79a350563977153309855013
Jordan Gregory <jordan.gregory@paynearme.com>
2023-12-05 16:57:06 -0600

    add day 3

---
+++ 2023/rust/day-3/src/bin/part2.rs
@@ -0,0 +1,8 @@
+use day_3::part2::process;
+
+fn main() -> anyhow::Result<()> {
+    let file = include_str!("../../input.txt");
+    let result = process(file)?;
+    println!("Result: {}", result);
+    Ok(())
+}
